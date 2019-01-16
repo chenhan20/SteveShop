031519a3806adf65fa256bea6241b12ceda34505
SteveChuang <steve.chuang@careline.com.tw>
2019-01-16 17:58:27 +0800

    test sass 未完成

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/public/dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.sass$/,
+      use: [{
+          loader: "style-loader" // 将 JS 字符串生成为 style 节点
+      }, {
+          loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+      }, {
+          loader: "sass-loader" // 将 Sass 编译成 CSS
+      }]
+    }]
   }
 }
