41fa1035029848614a3cbc5bf89292c0d43371da
SteveChuang <a0911558945@gmail.com>
2019-01-23 00:39:09 +0800

    fontawesome  新增

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 import 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 import '../css/index.sass'; // 這裡
 
     $('.steve').text('Jquery It works!');
